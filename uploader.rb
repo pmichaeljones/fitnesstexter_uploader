9b913012a26f45ff22c5464373a50d6af3717548
Patrick Jones <pmichaeljones@gmail.com>
2014-12-02 10:53:11 -0700

    added user input confirmations

--- uploader.rb
+++ uploader.rb
@@ -4,25 +4,33 @@ require 'pry'
 require 'json'
 require 'base64'
 
-print "What is the name of the file? (cfdog.pdf, cfbig.pdf, etc"
+print "What is the name of the file? (cfdog.pdf, cfbig.pdf, etc:\n"
 banner_file = gets.chomp
 
 content = File.read(banner_file)
 encoded = Base64.encode64(content)
 
-print "Recipient name:"
-name = gets.chomp
-print "Business Name:"
-business = gets.chomp
-print "Address Street:"
-address = gets.chomp
-print "City:"
-city = gets.chomp
-print "State:"
-state = gets.chomp
-print "Zip code:"
-zip = gets.chomp
+confirm = ""
 
+until confirm == "Y"
+  print "Recipient name:\n"
+  name = gets.chomp
+  print "Business Name:\n"
+  business = gets.chomp
+  print "Address Street:\n"
+  address = gets.chomp
+  print "City:\n"
+  city = gets.chomp
+  print "State:\n"
+  state = gets.chomp
+  print "Zip code:\n"
+  zip = gets.chomp
+
+  puts "-----------\n"
+  print "Full Mailing Address:\n\n#{name}\n#{business}\n#{address}\n#{city}, #{state} #{zip}\n\nConfirm address? (Y/N): "
+
+  confirm = gets.chomp
+end
 
 send_data = {
   "ReturnAddressId" => "southbroadway",
@@ -44,7 +52,7 @@ http = Net::HTTP.new(uri.host, uri.port)
 http.set_debug_output $stdout
 http.use_ssl = true
 
-request = Net::HTTP::Post.new(uri)
+request = Net::HTTP::Post.new(uri.request_uri)
 request['Content-Type'] = "application/json"
 request['Authorization'] = "TPTESTCF24A7D8095EDF88E3EFD6103C"
 request.body = send_data.to_json
